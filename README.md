76c24512c88c58fad7f5cfb7c845090d11e9bf1f
Nitish2910 <nitiskumar.g129@gmail.com>
2024-11-12 10:31:08 +0530

    deploy

---
+++ README.md
@@ -0,0 +1,8 @@
+Install Dependencies
+   npm install
+
+Run the Application
+   npm start
+
+Access the Application
+   Open your browser and go to `http://localhost:3000`.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './MainApp';
+
+const CreateRoot = ReactDOM.createRoot(document.getElementById('rootElement'));
+CreateRoot.render(
+    <App />
+);
+
